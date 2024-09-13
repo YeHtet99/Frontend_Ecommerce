730c937054a30d28f3f536c50988eced64a3e556
yemyinthtet <yemyinthtet99999@gmail.com>
2024-09-13 11:22:15 +0630

    connect railway

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,2 @@
-export const baseUrl= "http://localhost:8080/"
-// export const baseUrl = "https://backendecommerce-production-486d.up.railway.app/"
+// export const baseUrl= "http://localhost:8080/"
+export const baseUrl = "https://backendecommerce-production-486d.up.railway.app/"
